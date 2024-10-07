ab5191417507112f279bc7ae885b4352c1f6dbf5
b0tski <118492958+b0tski@users.noreply.github.com>
2024-10-07 17:36:11 -0400

    Update Program.cs
    Added many things 
    - Enemy will attack, parry, and heal
    - player can pass to dodge a parry
    - added health UI

--- Program.cs
+++ Program.cs
@@ -1,8 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace LarryLair
@@ -14,40 +15,67 @@ namespace LarryLair
 
             // PLayers Stats
             Entity player = new Entity();
-            player.name = "Botski";
+           
 
             // Create an Enemy 
             Entity larry = new Entity();
-            larry.health = 50;
-            int enemyCount = 0;
 
-            Console.WriteLine("Your goal is to Larry! Good luck!");
+            int delayTime = 1000;
+            Random ran = new Random();
+
+            Console.WriteLine("Your goal is to defeat Larry! Good luck!");
+
+            // Gets the players name 
+            Console.Write("Please enter a name for your animal\nEnter:");
+            string animalsName = Console.ReadLine();
+            player.name = animalsName;
 
 
             while (player.health >= 0 && larry.health >= 0)
             {
 
-                // larry's turn
-                DisplayPlayerStats(larry);
-
-                Action(larry, player, "2");
-
-                DisplayPlayerStats(larry);
-
-
                 // Players turn
 
                 DisplayPlayerStats(player);
 
-                Console.WriteLine("What is your action:");
-                string playerInput = Console.ReadLine();
-                Action(player, larry, playerInput);
+                if (!player.isStunned)
+                {
+                    Console.WriteLine("What is your action:");
+                    string playerInput = Console.ReadLine();
+                    Action(player, larry, playerInput);
+                }
+                else 
+                    Console.WriteLine($"{player.name} is stunned!");
+                    player.isStunned = false;
 
-                DisplayPlayerStats(player);
+                Thread.Sleep(delayTime);
+                Console.WriteLine();
+                Console.WriteLine("---------------------------------------------");
+                Console.WriteLine();
 
+                // Larry's Turn 
+
+
+                if (!larry.isStunned)
+                {
+
+                    if (larry.health <= 30) Action(larry, player, "Heal");
+
+                    int larrysMoves = ran.Next(1, 3);
+                    if (larrysMoves == 1) Action(larry, player, "Attack");
+                    else Action(larry, player, "Parry");
+                }
+
+                larry.isStunned = false;
+
+                DisplayPlayerStats(larry);
+
+                
+                Thread.Sleep(delayTime);
+                Console.WriteLine();
+                Console.WriteLine("---------------------------------------------");
+                Console.WriteLine();
             }
-
-
         }
 
         static void Action(Entity attacker, Entity opponent, dynamic input)
@@ -55,59 +83,43 @@ namespace LarryLair
 
             switch (input)
             {
+
                 case "Attack":
                 case "1":
-                    // Checks if attacker is stunned
-
-                    if (attacker.isStunned)
-                    {
-                        Console.WriteLine($"{attacker.name} is stunned!");
-                        attacker.isAttacking = false;
-                        attacker.isStunned = false;
-                    }
-                    else
-                    {
-                        attacker.health -= attacker.incomingDamage;
-                        attacker.incomingDamage = 0;
-
                         Console.WriteLine($"{attacker.name} is attacking!");
                         attacker.Attack(opponent);
-                    }
+                        break;
 
-                    break;
-
-                case "Perry":
+                case "Pass":
                 case "2":
-                    if (attacker.isStunned)
-                    {
-                        attacker.health -= attacker.incomingDamage;
-                        attacker.incomingDamage = 0;
+                        Console.WriteLine($"{attacker.name} is passing!");
+                        break;
 
-                        Console.WriteLine($"{attacker.name} is stunned!");
-                        attacker.isStunned = false;
-                    }
-                    else
-                    {
-                        Console.WriteLine($"{attacker.name} is perrying!");
-                        attacker.Perry(opponent);
-                        Console.WriteLine(opponent.isStunned);
-                    }
+                case "Heal":
+                case "3":
+                        attacker.Heal(attacker);
+                        break;
 
-                    break;
+                case "Parry":
+                case "4":
+                        Console.WriteLine($"{attacker.name} is perrying");
+                        attacker.Parry(opponent);
+                        break;
             }
+
+            // Applies damage to the opponent
+            if (!opponent.isStunned) opponent.health -= attacker.attackDamage;
         }
 
         static void DisplayPlayerStats(Entity entity) 
         {
             Console.WriteLine();
-            Console.WriteLine("_____STATS_____");
-            Console.WriteLine($"{entity.name}'s health: {entity.health} ");
-            Console.WriteLine($"Stunned: {entity.isStunned}");
-            Console.WriteLine($"Attacking: {entity.isAttacking}");
-            Console.WriteLine($"Perrying: {entity.isPerrying}");
-            Console.WriteLine();
-        }
 
+            Console.WriteLine($"_____{entity.name}_____");
+            Console.WriteLine();
+            Console.WriteLine($"Health: {String.Concat(Enumerable.Repeat("-", entity.health))}");
+            Console.WriteLine($"Stunned: {entity.isStunned}");
+        }
     }
 
     class Entity
@@ -116,7 +128,6 @@ namespace LarryLair
 
         public int health = 100;
         public int perryCount = 5;
-        public int attackCount = 15;
         public int attackDamage = 10;
         public int incomingDamage = 0;
         public int healCount = 3;
@@ -129,25 +140,36 @@ namespace LarryLair
         public void Attack(Entity opponent)
         {
             opponent.incomingDamage = attackDamage;
-
-            attackCount -= 1;
             isAttacking = true;
         }
 
-        public void Perry(Entity opponent)
+        public void Parry(Entity opponent)
         {
             // Checks if opponent is attacking
             if (opponent.isAttacking) 
             {
                 Console.WriteLine("Perried successful");
                 opponent.isStunned = true;
+                opponent.isAttacking = false;
+                health += opponent.attackDamage;
             }
             else 
             {
                 Console.WriteLine("Missed perry");
                 isStunned = true;
-                
             }
+        }
+
+        public void Heal(Entity user)
+        {
+            Random ran = new Random();
+            int minHeal = 2;
+            int maxHeal = 5;
+
+            int healAmount = ran.Next(minHeal, maxHeal) * 10;
+
+            user.health += healAmount;
+            if (user.health > 100) user.health = 100;
         }
     }
 }
